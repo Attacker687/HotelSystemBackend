6935c367fb6533cb8609877e359c9d410d365348
Attacker687 <1151575946@qq.com>
2025-03-31 23:51:18 +0800

    First commit

---
+++ src/main/java/com/winniethepooh/hotelsystembackend/HotelSystemBackendApplication.java
@@ -0,0 +1,13 @@
+package com.winniethepooh.hotelsystembackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelSystemBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelSystemBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/winniethepooh/hotelsystembackend/HotelSystemBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.winniethepooh.hotelsystembackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelSystemBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
